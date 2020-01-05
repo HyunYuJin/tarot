68feb2a5a11924abec940ac6481b48581af18a29
YujinHyeon <gusdbwls9772@gmail.com>
2020-01-05 23:31:35 +0900

    Reading again

--- app/components/Reading.vue
+++ app/components/Reading.vue
@@ -1,14 +1,16 @@
 <template>
 <StackLayout>
     <Label row="0" text="My Reading" class="title med" />
-    <GridLayout rows="auto,1,*" columns="*" class="card">
-        <GridLayout row="0" rows="*" columns="*,*,*">
+    <GridLayout rows="auto, 1, *" columns="*" class="card">
+        <GridLayout row="0" rows="*" columns="*, *, *">
             <Label col="0" :class="tabButtonClasses('Past')" text="PAST" @tap="getCard('Past')" />
             <Label col="1" :class="tabButtonClasses('Present')" text="PRESENT" @tap="getCard('Present')" />
             <Label col="2" :class="tabButtonClasses('Future')" text="FUTURE" @tap="getCard('Future')" />
         </GridLayout>
+
         <StackLayout row="1" backgroundColor="#8089A8" style="opacity: .2"></StackLayout>
-        <GridLayout row="2" rows="2*,3*,3*" class="card">
+        
+        <GridLayout row="2" rows="2* ,3*, 3*" class="card">
             <Label row="0" textWrap="true" class="card-title" :text="name" />
             <Image row="1" v-if="major" :class="icon" :src="emoji" />
             <StackLayout row="1" rows="*" columns="*,*" v-if="!major" horizontalAlignment="center" orientation="horizontal">
